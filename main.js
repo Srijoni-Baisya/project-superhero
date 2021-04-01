ea63dd39a719179f7218ed2f13c6b65f38995333
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-04-01 12:04:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ var block_object="";
 function player_update(){
     fabric.Image.fromURL("superhero.png",function(Img){
         player_object=Img;
-        player_object.setToWidth(100);
-        player_object.setToHeight(170);
+        player_object.scaleToWidth(100);
+        player_object.scaleToHeight(170);
         player_object.set({
             top:player_y,
             left:player_x
@@ -22,12 +22,80 @@ function player_update(){
 function new_image(get_image){
     fabric.Image.fromURL(get_image,function(Img){
         block_object=Img;
-        block_object.setToWidth(30);
-        block_object.setToHeight(30);
+        block_object.scaleToWidth(block_width);
+        block_object.scaleToHeight(block_height);
         block_object.set({
             top:player_y,
             left:player_x
         });
         canvas.add(block_object);
     });
+}
+
+window.addEventListener("keydown",my_keydown);
+
+function my_keydown(e){
+    keyPressed=e.keyCode;
+    console.log(keyPressed);
+
+    if(e.shiftKey==true && keyPressed=='80'){
+        console.log("p and shift pressed together");
+        block_width=block_width+10;
+        block_height=block_height+10;
+        document.getElementById("current_width").innerHTML=block_width;
+        document.getElementById("current_height").innerHTML=block_height;
+    }
+
+    if(e.shiftKey==true && keyPressed=='77'){
+        console.log("m and shift pressed together");
+        block_width=block_width-10;
+        block_height=block_height-10;
+        document.getElementById("current_width").innerHTML=block_width;
+        document.getElementById("current_height").innerHTML=block_height;
+    }
+
+    if(keyPressed=='70'){
+        new_image("iron_man_face.png");
+        console.log("f");
+    }
+
+    if(keyPressed=='66'){
+        new_image("spiderman_body.png");
+        console.log("b");
+    }
+
+    if(keyPressed=='76'){
+        new_image("hulk_legs.png");
+        console.log("l");
+    }
+
+    if(keyPressed=='82'){
+        new_image("thor_right_hand.png");
+        console.log("r");
+    }
+
+    if(keyPressed=='72'){
+        new_image("captain_america_left_hand.png");
+        console.log("h");
+    }
+
+    if(keyPressed=='38'){
+        //up();
+        console.log("up");
+    }
+
+    if(keyPressed=='40'){
+        //down();
+        console.log("down");
+    }
+
+    if(keyPressed=='37'){
+        //left();
+        console.log("left");
+    }
+
+    if(keyPressed=='39'){
+        //right();
+        console.log("right");
+    }
 }
